56bc0f3be2b53b01161074f3ae7fde07d9afbd53
Hosein <hshafie512@gmail.com>
2026-06-24 18:35:33 +0330

    Operator ++ , __ | prefix and postfix , Decrement and Increment |Example

--- js.js
+++ js.js
@@ -1,33 +1,54 @@
-// استفاده از اپراتور + در نامبر ها
-let a = 5 + 10;
-console.log(a);
-console.log(typeof a);
+// operator | ++ | --
+
+//prefix Increment | پیش افزایش
+let a = 10;
+console.log(++a);
+
+// prefix Decrement | پیش کاهش
+let b = 10;
+console.log(--b);
+
+//postfix Increment | پس افزایش
+let c = 10;
+console.log(c++);
+
+//postfix Decrement | پس کاهش
+let d = 10;
+console.log(d--);
 
 
-//استفاده از اپراتور + در استرینگ ها
-let b = '5' + '10';
-console.log(b);
-console.log(typeof b);
+//example
 
-let c = 'hosein';
-let d = 'shafie';
-let e = c + d;
-console.log(e);
-console.log(typeof e);
+//prefix Increment
+let e = 20;
+console.log(e);  //خروجی 20
+console.log(++e); // خروجی 21 
+console.log(e);  // خروجی 21
 
-//استفاده از اپراتور + بین نامبر و استرینگ که خروجی استرینگ بهمون میده
-//اگر نامبر قبل از رشته قرار بگیره نامبر ها جمع میشن و به رشته میچسبن
-let f = 5 + 6 + 'hosein';
-console.log(f);
-console.log(typeof f);
-
-//اگر نامبر ها بعد از رشته قرار بگیرن بصورت استرینگ باهاشون برخورد میشه و خروجی مقدار رشته با عدد ها کنارش قرار میگیره
-let g = 'hosein' + 5 + 6;
+//postfix Increment 
+let g = 40;
+console.log(g); // خروجی 40
+console.log(g++); //
 console.log(g);
-console.log(typeof g);
-console.log(length.g);
+console.log(g * 2);
 
- 
+//prefix Decrement
+let f = 30;
+console.log(f);
+console.log(--f);
+console.log(f);
+
+
+//postfix Decrement
+let h = 50;
+console.log(h);
+console.log(h--);
+console.log(h);
+console.log(h * 2);
+
+
+
+
 
 
 
