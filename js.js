54db89274300dcbd6f581398689505980a04340d
Hosein <hshafie512@gmail.com>
2026-06-24 19:12:11 +0330

    Operator precedence

--- js.js
+++ js.js
@@ -1,50 +1,26 @@
-// operator | ++ | --
+// Operator precedence | تقدم اپراتور ها
 
-//prefix Increment | پیش افزایش
-let a = 10;
-console.log(++a);
+// () > ** > * > / > % > + > -
 
-// prefix Decrement | پیش کاهش
-let b = 10;
-console.log(--b);
+//Example
 
-//postfix Increment | پس افزایش
-let c = 10;
-console.log(c++);
+let a = 2 + 3 * 4;
+console.log(a);
 
-//postfix Decrement | پس کاهش
-let d = 10;
-console.log(d--);
+let b = (2 + 3) * 4;
+console.log(b);
 
+let c = 2 * 3 ** 2;
+console.log(c);
 
-//example
-
-//prefix Increment
-let e = 20;
-console.log(e);  //خروجی 20
-console.log(++e); // خروجی 21 
-console.log(e);  // خروجی 21
-
-//postfix Increment 
-let g = 40;
-console.log(g); // خروجی 40
-console.log(g++); //
-console.log(g);
-console.log(g * 2);
-
-//prefix Decrement
-let f = 30;
-console.log(f);
-console.log(--f);
-console.log(f);
+let d = 2 + 3 * 5 ** 2;
+console.log(d);
 
 
-//postfix Decrement
-let h = 50;
-console.log(h);
-console.log(h--);
-console.log(h);
-console.log(h * 2);
+// source link for reading "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_precedence"
+
+
+
 
 
 
