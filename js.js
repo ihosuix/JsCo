22c44a5ecc6972700d106d3120dfebaf3f388873
Hosein <hshafie512@gmail.com>
2026-07-02 20:00:23 +0330

    Practice with the `if` conditional structure and input values

--- js.js
+++ js.js
@@ -1,2 +1,10 @@
-let firstname = document.getElementById("name");
-console.log(firstname.value);
+let button = document.getElementById("button");
+let ageInput = document.getElementById("age");
+
+button.onclick = function() {
+    if(ageInput.value >= 18) {
+        alert("شما می توانید وارد شوید");
+    } if(ageInput.value < 18) {
+        alert("شما نمی توانید وارد شوید");
+    }
+}
